7a6b0efff298b5e773b5398cf1b8041a46d546a0
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-03-16 16:53:06 +0300

    docs: updates on workflows
    Added information on documentation

--- src/numbers.ts
+++ src/numbers.ts
@@ -169,11 +169,11 @@ export namespace Numbers {
 
         return Math.abs(
             Math.floor(n / 7) +
-            Math.floor(n / 3) -
-            2 * Math.floor(n / 21) -
-            Math.floor((m - 1) / 7) -
-            Math.floor((m - 1) / 3) +
-            2 * Math.floor((m - 1) / 21),
+                Math.floor(n / 3) -
+                2 * Math.floor(n / 21) -
+                Math.floor((m - 1) / 7) -
+                Math.floor((m - 1) / 3) +
+                2 * Math.floor((m - 1) / 21),
         )
     }
 
