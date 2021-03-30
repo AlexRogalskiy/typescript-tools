df8bfe2c394c711723dc4fad5a958814303117dd
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-03-30 23:59:24 +0300

    docs: updates on workflows
    Added information on documentation

--- tests/logger.ts
+++ tests/logger.ts
@@ -1,25 +1,17 @@
-const loggerLevels: string[] = [
-    'trace',
-    'debug',
-    'info',
-    'warn',
-    'error',
-    'fatal',
-    'child',
-];
-const logger: any = {};
+const loggerLevels: string[] = ['trace', 'debug', 'info', 'warn', 'error', 'fatal', 'child']
+const logger: any = {}
 
 loggerLevels.forEach(k => {
-    logger[k] = jest.fn();
-});
+    logger[k] = jest.fn()
+})
 
-export const setContext = jest.fn();
-export const getContext = jest.fn();
-export const setMeta = jest.fn();
-export const addMeta = jest.fn();
-export const removeMeta = jest.fn();
-export const levels = jest.fn();
-export const addStream = jest.fn();
-export const getProblems = jest.fn((): any[] => []);
+export const setContext = jest.fn()
+export const getContext = jest.fn()
+export const setMeta = jest.fn()
+export const addMeta = jest.fn()
+export const removeMeta = jest.fn()
+export const levels = jest.fn()
+export const addStream = jest.fn()
+export const getProblems = jest.fn((): any[] => [])
 
-export { logger };
+export { logger }
