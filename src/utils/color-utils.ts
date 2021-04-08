eedbb5ceb10bfa4df6cda90389807f9e7bca4e27
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-04-09 01:36:10 +0300

    gi

--- src/utils/color-utils.ts
+++ src/utils/color-utils.ts
@@ -71,6 +71,34 @@ export namespace ColorsUtils {
         }
     }
 
+    export const lightenDarkenColor = (col: string, amt: number): string => {
+        let usePound = false
+
+        if (col.startsWith('#')) {
+            col = col.slice(1)
+            usePound = true
+        }
+
+        const num = parseInt(col, 16)
+
+        let r = (num >> 16) + amt
+
+        if (r > 255) r = 255
+        else if (r < 0) r = 0
+
+        let b = ((num >> 8) & 0x00ff) + amt
+
+        if (b > 255) b = 255
+        else if (b < 0) b = 0
+
+        let g = (num & 0x0000ff) + amt
+
+        if (g > 255) g = 255
+        else if (g < 0) g = 0
+
+        return (usePound ? '#' : '') + (g | (b << 8) | (r << 16)).toString(16)
+    }
+
     export const getBrightness2 = (color: string): number => {
         const rgb = convertHexStringToRgbString(color)
         return (rgb.r * 299 + rgb.g * 587 + rgb.b * 114) / 1000
