eb4cbde3074fa7cd9fbf9389762a3b3f36231fd9
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-04-16 00:38:42 +0300

    docs: updates on workflows
    Added information on documentation

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 export * from './config/configs'
+export * from './config/profiles'
+
+export * from './constant/constants'
 
 export * from './utils/easing-utils'
-export * from './utils/calc-utils'
+export * from './utils/color-utils'
 export * from './utils/common-utils'
+export * from './utils/calc-utils'
+export * from './utils/exec-utils'
 export * from './utils/translation-utils'
 
 export * from './logging/logging'
+
 export * from './error/errors'
 
 export * from './datetime/datetimes'
+export * from './datetime/sleep'
+export * from './datetime/time'
 
 export * from './validation/checkers'
 export * from './validation/validators'
@@ -22,9 +30,10 @@ export * from './utils/exec-utils'
 
 export * from './types/arraybuffers'
 export * from './types/arrays'
+export * from './types/events'
+export * from './types/functions'
 export * from './types/objects'
 export * from './types/regexes'
-export * from './types/functions'
 export * from './types/strings'
 
 export * from './io/files'
@@ -33,6 +42,10 @@ export * from './network/requests'
 export * from './network/browsers'
 export * from './network/clients'
 export * from './network/networks'
+export * from './network/address'
+export * from './network/canvas'
+export * from './network/keyframes'
+export * from './network/url'
 
 export * from './mediator/sorting'
 export * from './mediator/mixins'
@@ -40,3 +53,5 @@ export * from './mediator/formats'
 export * from './mediator/commons'
 export * from './mediator/comparators'
 export * from './mediator/converters'
+export * from './mediator/decorators'
+export * from './mediator/predicates'
