5bddc1b06f344b754c9848c17ddcad5589b9ea5a
BFT\a.rogalskii <a.rogalskii@bftcom.com>
2021-07-01 14:13:26 +0300

    Added info on workflows
    Updates on github-actions

--- scripts/tag-latest.js
+++ scripts/tag-latest.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 
-const cp = require('child_process');
-const argv = require('yargs').argv;
+const cp = require('child_process')
+const argv = require('yargs').argv
 
-const fromImageTag = argv.fromImageTag || 'latest';
+const fromImageTag = argv.fromImageTag || 'latest'
 
-const fromImage = `cedalo/streamsheets:${fromImageTag}`;
-const toImage = `cedalo/streamsheets:latest`;
+const fromImage = `cedalo/streamsheets:${fromImageTag}`
+const toImage = `cedalo/streamsheets:latest`
 
 try {
-    console.log(`Pulling image ${fromImage}`);
-    cp.execSync(`docker pull ${fromImage}`, {stdio: 'inherit'});
-    console.log(`Tagging image ${toImage}`);
-    cp.execSync(`docker tag ${fromImage} ${toImage}`, {stdio: 'inherit'});
-    console.log(`Pushing image ${toImage}`);
-    cp.execSync(`docker push ${toImage}`, {stdio: 'inherit'});
+    console.log(`Pulling image ${fromImage}`)
+    cp.execSync(`docker pull ${fromImage}`, { stdio: 'inherit' })
+    console.log(`Tagging image ${toImage}`)
+    cp.execSync(`docker tag ${fromImage} ${toImage}`, { stdio: 'inherit' })
+    console.log(`Pushing image ${toImage}`)
+    cp.execSync(`docker push ${toImage}`, { stdio: 'inherit' })
 } catch (error) {
-    process.exit(1);
+    process.exit(1)
 }
