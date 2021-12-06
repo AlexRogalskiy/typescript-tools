2c79c88141598f197f0252df0241c9220b987f2b
Alexander Rogalskiy <arogalskii@sbereducation.ru>
2021-12-06 05:25:41 +0300

    Updates on files

---
+++ src/annotation/readme-parser.ts
@@ -0,0 +1,53 @@
+import regexJoin from 'regex-join';
+import {readFileSync} from 'node:fs';
+import {parse as parseMarkdown} from 'markdown-wasm/dist/markdown.node.js';
+
+// Group names must be unique because they will be merged
+const simpleFeatureRegex = /^- \[]\(# "(?<simpleId>[^"]+)"\)(?: 🔥)? (?<simpleDescription>.+)$/gm;
+const highlightedFeatureRegex = /<p><a title="(?<highlightedId>[^"]+)"><\/a> (?<highlightedDescripion>.+?)\n\t+<p><img src="(?<highlightedImage>.+?)">/g;
+// eslint-disable-next-line unicorn/better-regex -- ur wrong
+const featureRegex = regexJoin(simpleFeatureRegex, /|/, highlightedFeatureRegex);
+
+function extractDataFromMatch(match: RegExpMatchArray): FeatureMeta {
+    const {
+        simpleId,
+        simpleDescription,
+        highlightedId,
+        highlightedDescripion,
+        highlightedImage,
+    } = match.groups!;
+    if (highlightedId) {
+        return {
+            id: highlightedId as FeatureID,
+            description: parseMarkdown(highlightedDescripion + '.'),
+            screenshot: highlightedImage,
+        };
+    }
+
+    const urls: string[] = [];
+    const urlExtracter = (_match: string, title: string, url: string): string => {
+        urls.push(url);
+        return title;
+    };
+
+    const linkLessMarkdownDescription = simpleDescription.replace(/\[(.+?)]\((.+?)\)/g, urlExtracter);
+    return {
+        id: simpleId as FeatureID,
+        description: parseMarkdown(linkLessMarkdownDescription),
+        screenshot: urls.find(url => /\.(png|gif)$/i.test(url)),
+    };
+}
+
+export function getFeaturesMeta(): FeatureMeta[] {
+    const readmeContent = readFileSync('readme.md', 'utf-8');
+    return [...readmeContent.matchAll(featureRegex)]
+        .map(match => extractDataFromMatch(match))
+        .sort((firstFeature, secondFeature) => firstFeature.id.localeCompare(secondFeature.id));
+}
+
+export function getImportedFeatures(): FeatureID[] {
+    const contents = readFileSync('source/refined-github.ts', 'utf-8');
+    return [...contents.matchAll(/^import '\.\/features\/([^.]+)';/gm)]
+        .map(match => match[1] as FeatureID)
+        .sort();
+}
---
+++ src/annotation/verify-features.ts
@@ -0,0 +1,70 @@
+import {existsSync, readdirSync, readFileSync} from 'node:fs';
+
+import {getImportedFeatures, getFeaturesMeta} from './readme-parser.js'; // Must import as `.js`
+
+const featuresDirContents = readdirSync('source/features/');
+const entryPoint = 'source/refined-github.ts';
+const entryPointSource = readFileSync(entryPoint);
+const importedFeatures = getImportedFeatures();
+const featuresInReadme = getFeaturesMeta();
+
+function findCssFileError(filename: string): string | void {
+    const isImportedByEntrypoint = entryPointSource.includes(`import './features/${filename}';`);
+    const correspondingTsxFile = `source/features/${filename.replace(/.css$/, '.tsx')}`;
+    if (existsSync(correspondingTsxFile)) {
+        if (!readFileSync(correspondingTsxFile).includes(`import './${filename}';`)) {
+            return `ERR: \`${filename}\` should be imported by \`${correspondingTsxFile}\``;
+        }
+
+        if (isImportedByEntrypoint) {
+            return `ERR: \`${filename}\` should only be imported by \`${correspondingTsxFile}\`, not by \`${entryPoint}\``;
+        }
+
+        return;
+    }
+
+    if (!isImportedByEntrypoint) {
+        return `ERR: \`${filename}\` should be imported by \`${entryPoint}\` or removed if it is not needed`;
+    }
+}
+
+function findError(filename: string): string | void {
+    if (filename === 'index.tsx') {
+        return;
+    }
+
+    if (filename.endsWith('.css')) {
+        return findCssFileError(filename);
+    }
+
+    if (!filename.endsWith('.tsx')) {
+        return `ERR: The \`/source/features\` folder should only contain .css and .tsx files. File \`${filename}\` violates that rule`;
+    }
+
+    const featureId = filename.replace('.tsx', '') as FeatureID;
+    if (!importedFeatures.includes(featureId)) {
+        return `ERR: ${featureId} should be imported by \`${entryPoint}\``;
+    }
+
+    // The previous checks apply to RGH features, but the next ones don't
+    if (filename.startsWith('rgh-')) {
+        return;
+    }
+
+    const [featureMeta, duplicate] = featuresInReadme.filter(feature => feature.id === featureId);
+    if (!featureMeta) {
+        return `ERR: ${featureId} should be described in the readme`;
+    }
+
+    if (featureMeta.description.length < 20) {
+        return `ERR: ${featureId} should be described better in the readme (at least 20 characters)`;
+    }
+
+    if (duplicate) {
+        return `ERR: ${featureId} should be described only once in the readme`;
+    }
+}
+
+const errors = featuresDirContents.map(name => findError(name)).filter(Boolean);
+console.error(errors.join('\n'));
+process.exitCode = errors.length;
